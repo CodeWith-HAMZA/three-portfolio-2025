a63c05661aa1dd8881403a9daf216bb849a6d0b6
Hamza <hamza263973@gmail.com>
2025-01-23 12:28:08 +0500

    updated

--- threejs-portfolio-main/src/sections/Hero.jsx
+++ threejs-portfolio-main/src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Hamza <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Building Products With React JS </p>
+        <p className="hero_tag text-gray_gradient">Building Products and Brands </p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
